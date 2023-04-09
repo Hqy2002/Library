7d606da8765df572158b42271f4f930df347ab8d
z <23300549455@qq.com>
2023-04-09 18:27:22 +0800

    变更

---
+++ src/main/java/com/library/Mybatis/MybatisUtil.java
@@ -0,0 +1,19 @@
+package com.library.Mybatis;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MybatisUtil {
+
+    public static SqlSession getSqlSession() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        return sqlSessionFactory.openSession();
+    }
+}
---
+++ src/main/java/com/library/Student.java
@@ -0,0 +1,11 @@
+package com.library;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    int id ;
+    String name;
+    int sex;
+    int age;
+}
